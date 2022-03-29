f1ac2a32333e883462cce5682ed4a8b1100da58a
PeagnoLuc05 <78964140+PeagnoLuc05@users.noreply.github.com>
2022-03-29 08:11:52 -0300

    Create codigo.ino

---
+++ aula3/codigo.ino
@@ -0,0 +1,16 @@
+const int led = 3;
+const int pot = 0;
+  
+void setup()
+{
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+int var;
+void loop()
+{
+  var = analogRead(pot);
+  Serial.println(var); 
+  delay(100);
+}
